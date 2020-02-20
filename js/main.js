3c26122a91e1330ea9e7e2af425ab16e0912beac
klahoda <54544806+klahoda@users.noreply.github.com>
2020-02-20 10:27:02 -0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,53 @@
+function isEven(value) {
+	if (value%2 == 0)
+		return true;
+	else
+		return false;
+}
+
+$(document).ready(function() {
+
+	$(".thumb").click(function() {
+
+		var e = $('.thumb').index(this);
+		var me = $(this);
+		var nex = $(this).next();
+
+		var gutter = parseInt($(this).css("fontSize"))*2;
+		console.log(gutter);
+
+		$( ".overlay-holder .overlay .body" ).empty();
+		$(".overlay-holder").removeClass('active');
+		$(".overlay-holder").addClass('hidden');
+		$(".overlay").removeClass('overlay-even');
+		$(".overlay").removeClass('overlay-odd');
+
+		$(this).find(".body").clone(true, true).contents().appendTo('.overlay-holder .overlay .body');
+
+		if(isEven(e)){
+			$(".overlay-holder").insertAfter(nex);
+			$(".overlay").addClass('overlay-even');
+		}else{
+			$(".overlay-holder").insertAfter(me);
+			$(".overlay").addClass('overlay-odd');
+		}
+
+		$(".overlay-holder").toggleClass('active hidden');
+
+		//window.scrollTo({top: 0, behavior: 'smooth'});
+		$('html,body').animate({
+        scrollTop: $(".overlay-holder").offset().top-gutter},
+        'slow');
+
+	});
+
+	$("#close").click(function() {
+
+		$( ".overlay-holder .overlay .body" ).empty();
+
+		$(".overlay-holder").removeClass('active');
+		$(".overlay-holder").addClass('hidden');
+
+	});
+
+});
